2aef750669dfa57268721bce863d4b3cd71d1212
Mike Gardner <mjgardner@abitofhelp.com>
2023-12-17 00:13:05 -0700

    init

--- scripts/status.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Short commit id
-echo STABLE_GIT_COMMIT $(git describe --always)
-
-# Full commit id
-#echo "STABLE_GIT_COMMIT $(git rev-parse HEAD)"
--- server/main.go
+++ server/main.go
@@ -1,37 +1,41 @@
 package main
 
-import (
-	"log"
-	"net"
-
-	pb "github.com/abitofhelp/bazel7_go_grpc"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-)
-
-const (
-	port = ":50051"
-)
-
-// server is used to implement helloworld.GreeterServer.
-type server struct{}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
-}
-
+// import (
+//
+//	"log"
+//	"net"
+//
+//	pb "github.com/abitofhelp/bazel7_go_grpc"
+//	"golang.org/x/net/context"
+//	"google.golang.org/grpc"
+//	"google.golang.org/grpc/reflection"
+//
+// )
+//
+// const (
+//
+//	port = ":50051"
+//
+// )
+//
+// // server is used to implement helloworld.GreeterServer.
+// type server struct{}
+//
+// // SayHello implements helloworld.GreeterServer
+//
+//	func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+//		return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+//	}
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	//	lis, err := net.Listen("tcp", port)
+	//	if err != nil {
+	//		log.Fatalf("failed to listen: %v", err)
+	//	}
+	//	s := grpc.NewServer()
+	//	pb.RegisterGreeterServer(s, &server{})
+	//	// Register reflection service on gRPC server.
+	//	reflection.Register(s)
+	//	if err := s.Serve(lis); err != nil {
+	//		log.Fatalf("failed to serve: %v", err)
+	//	}
 }
